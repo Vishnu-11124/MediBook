51bd996170ba674ea0d9f57a6a997e12e2106702
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-07-30 12:27:28 +0530

    Add filtering functionality to Doctors page for improved doctor selection based on speciality

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import DoctorCard from '../components/DoctorCard'
 
 const Doctors = () => {
+
+  const { speciality } = useParams()
+  const [filterDoc, setFilterDoc] = useState([])
+
+  const { doctors } = useContext(AppContext)
+
+  const applyFilter = () => {
+  setFilterDoc(
+    speciality
+      ? doctors.filter((doc) => doc.speciality === speciality)
+      : doctors
+  );
+};
+
+  useEffect(() => {
+    applyFilter()
+  },[doctors,speciality])
+
+
   return (
     <div>
-      
+      <p>Browse through the doctors specialist.</p>
+      <div>
+        <div>
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        <div>
+          {
+            filterDoc.map((doctor) => (
+              <DoctorCard key={doctor._id} doctor={doctor} />
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
