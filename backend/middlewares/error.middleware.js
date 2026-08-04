5f070e0e30ca3fba42d0735b34d4c18c7e7045e7
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-08-04 22:29:12 +0530

    feat: initialize backend with Express, add API error and response utilities
    - Created package.json for backend dependencies and scripts.
    - Implemented server.js to set up Express server with CORS and JSON parsing.
    - Added ApiError utility for standardized error handling.
    - Added ApiResponse utility for consistent API responses.
    - Implemented asyncHandler utility for handling asynchronous route handlers.

---
+++ backend/middlewares/error.middleware.js
@@ -0,0 +1,10 @@
+const errorHandler = (err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    success: false,
+    statusCode: err.statusCode || 500,
+    message: err.message || "Internal Server Error",
+    errors: err.errors || []
+  });
+};
+
+export default errorHandler;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config.js'
+import errorHandler from './middlewares/error.middleware.js'
+
+
+const app = express()
+const port = process.env.PORT || 4000
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.get("/", (req, res) => {
+    res.send("Api working...")
+})
+
+// error middleware
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
+})
