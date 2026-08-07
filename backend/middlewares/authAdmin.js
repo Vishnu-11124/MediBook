f094ae5a1a69721b65b99682be9162e759f12334
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-08-07 12:54:34 +0530

    feat: add admin authorization middleware to addDoctor route

---
+++ backend/middlewares/authAdmin.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/ApiError.js";
+
+export const isAdmin = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            throw new ApiError(401, "Unauthorized access");
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (decoded.role !== "admin") {
+            throw new ApiError(401, "Unauthorized");
+        }
+
+        req.admin = decoded;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addDoctor, adminLogin } from "../controllers/adminController.js";
 import uplpoad from "../middlewares/multer.js";
+import { isAdmin } from "../middlewares/authAdmin.js";
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-doctor', uplpoad.single('image'), addDoctor)
+adminRouter.post('/add-doctor', isAdmin, uplpoad.single('image'), addDoctor)
 
 adminRouter.post('/login', adminLogin)
 
