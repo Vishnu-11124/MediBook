fcb01f54ca713a437b6382a38365affede8799e9
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-08-14 11:54:05 +0530

    feat: integrate react-toastify for notifications and update doctors data fetching logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,13 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,19 +1,42 @@
 import { createContext } from "react";
-import { doctors } from "../assets/assets";
+// import { doctors } from "../assets/assets";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    
-    const value = {
-        doctors
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const [doctors, setDoctors] = useState([]);
+
+  const value = {
+    doctors,
+  };
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/all-doctors");
+      if (data.success) {
+        // console.log(data)
+        setDoctors(data?.data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
 
-}
-export default AppContextProvider
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+export default AppContextProvider;
