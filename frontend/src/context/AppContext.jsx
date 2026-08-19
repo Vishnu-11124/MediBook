0a383db3eb50d08ced3f9ccf4d2f2ae496d9e1dd
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-08-19 11:48:10 +0530

    feat: add user profile data loading functionality to AppContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -14,6 +14,8 @@ const AppContextProvider = (props) => {
 
   const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false )
 
+  const [userData, setUserData] = useState(false)
+
   const getDoctorsData = async () => {
     try {
       const { data } = await axios.get(backendUrl + "/api/doctor/all-doctors");
@@ -29,13 +31,41 @@ const AppContextProvider = (props) => {
     }
   };
 
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      // console.log(data);
+
+      if (data.success) {
+        setUserData(data.data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || error.message);
+    }
+  };
+
   const value = {
-    doctors, token, setToken, backendUrl
+    doctors, token, setToken, backendUrl, userData, setUserData, loadUserProfileData
   };
 
   useEffect(() => {
     getDoctorsData();
   }, []);
+
+  useEffect(() => {
+    if(token){
+      loadUserProfileData()
+    }else{
+      setUserData(false)
+    }
+  },[token])
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
