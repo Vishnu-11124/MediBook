48632834f26f7d8aa1a07fa6d51036a555471900
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-08-23 20:07:38 +0530

    feat: implement doctor details fetching with availability using doctorId

--- admin/src/pages/Admin/DoctorDetails.jsx
+++ admin/src/pages/Admin/DoctorDetails.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { AdminContext } from "../../context/AdminContext";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const DoctorDetails = () => {
-  return (
-    <div>
-      <h1>doctor</h1>
-    </div>
-  )
-}
+  const { token, backendUrl } = useContext(AdminContext);
 
-export default DoctorDetails
+  const { doctorId } = useParams();
+
+  const [doctorData, setDoctorData] = useState(null);
+  const [availability, setAvailability] = useState(null);
+
+  const getDoctorDetails = async () => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + `/api/admin/doctors/${doctorId}/doctor-details`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      if (data.success) {
+        // console.log("data:", availability);
+        setDoctorData(data?.data?.doctorData);
+        setAvailability(data?.data?.availability);
+      } else {
+        toast.error(data.message);
+        console.log("Error:", data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+      console.log("Error:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getDoctorDetails();
+  }, [doctorId, token]);
+
+  return (
+    doctorData && (
+      <div>
+        <h1>doctor</h1>
+      </div>
+    )
+  );
+};
+
+export default DoctorDetails;
