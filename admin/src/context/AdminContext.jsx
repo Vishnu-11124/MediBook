c70ebed2b6881e8f323983f459a6d7d833fa6570
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-09-08 11:21:27 +0530

    feat: implement getAllAppointments function and update AdminContextProvider

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,45 +1,75 @@
 import { createContext, useState } from "react";
-import axios from 'axios'
-import {toast} from 'react-toastify'
+import axios from "axios";
+import { toast } from "react-toastify";
 
-export const AdminContext = createContext()
+export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : "",
+  );
+  const [doctors, setDoctors] = useState([]);
+  const [appointmentsList, setAppointmentsList] = useState([]);
 
-    const [token, setToken] = useState(localStorage.getItem('token')? localStorage.getItem('token'): '')
-    const [doctors, setDoctors] = useState([])
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-
-    const getAllDoctors = async () => {
-        try {
-            const {data} = await axios.get(backendUrl + '/api/admin/all-doctors',{
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            if(data.success) {
-                setDoctors(data?.data)
-                console.log(data?.data)
-            }else{
-                toast.error(data.message)
-            }
-        } catch (error) {
-            toast.error(error.message)
-        }
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/admin/all-doctors", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (data.success) {
+        setDoctors(data?.data);
+        console.log(data?.data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    const value = {
-        token, setToken,
-        backendUrl,
-        doctors, getAllDoctors
+  const getAllAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + "/api/admin/all-appointments",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      if (data.success) {
+        setAppointmentsList(data?.data);
+      } else {
+        toast.error(data.message);
+        console.log(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+      console.log(error.message);
     }
+  };
 
-    return (
-        <AdminContext.Provider value={value}>
-            {props.children}
-        </AdminContext.Provider>
-    )
-}
+  const value = {
+    token,
+    setToken,
+    backendUrl,
+    doctors,
+    getAllDoctors,
+    appointmentsList,
+    setAppointmentsList,
+    getAllAppointments,
+  };
 
-export default AdminContextProvider
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContextProvider;
