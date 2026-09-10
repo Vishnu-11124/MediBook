b4085a12788a8163fe8d39ce5e17724f49b74f67
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-09-10 11:01:41 +0530

    feat: implement doctor login functionality with JWT token generation and email validation

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -2,6 +2,8 @@ import DoctorModel from "../models/doctorModel.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const getAllDoctors = asyncHandler(async (req, res) => {
   const doctors = await DoctorModel.find({}).select("-password -email");
@@ -20,36 +22,52 @@ export const getAllDoctors = asyncHandler(async (req, res) => {
 // doctor login
 export const doctorLogin = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
     throw new ApiError(400, "Email and password are required");
   }
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (!emailRegex.test(email)) {
+  if (!emailRegex.test(email.trim())) {
     throw new ApiError(400, "Invalid email format");
   }
 
-  const doctorData = await DoctorModel.findOne({ email: email.trim() });
-  if (!doctorData) {
-    throw new ApiError(404, "Doctor not found");
-  }
-
-  if (doctorData.password !== password) {
-    throw new ApiError(401, "Invalid password");
-  }
-
-  const docToken = jwt.sign({ role: "doctor" }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+  const doctorData = await DoctorModel.findOne({
+    email: email.trim().toLowerCase(),
   });
 
-  res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        { token: docToken },
-        "Doctor logged in successfully",
-      ),
-    );
+  if (!doctorData) {
+    throw new ApiError(401, "Invalid email or password");
+  }
+
+  const isMatchingPassword = await bcrypt.compare(
+    password,
+    doctorData.password,
+  );
+
+  if (!isMatchingPassword) {
+    throw new ApiError(401, "Invalid email or password");
+  }
+
+  const docToken = jwt.sign(
+    {
+      id: doctorData._id,
+      role: "doctor",
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        token: docToken,
+      },
+      "Doctor logged in successfully",
+    ),
+  );
 });
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getAllDoctors } from '../controllers/doctorController.js'
+import { doctorLogin, getAllDoctors } from '../controllers/doctorController.js'
 
 const doctorRouter = express.Router()
 
 doctorRouter.get('/all-doctors', getAllDoctors)
+
+doctorRouter.post('/doctor-login', doctorLogin)
 
 export default doctorRouter
