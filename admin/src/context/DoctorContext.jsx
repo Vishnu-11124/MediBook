3272e0473acc7535a29b3c4ee01372c413879ece
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-09-11 21:17:11 +0530

    feat: implement appointment fetching and history management in DoctorContext and DoctorAppointments

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 
@@ -10,10 +12,49 @@ const DoctorContextProvider = (props) => {
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "",
   );
 
+  const [appointmentsList, setAppointmentsList] = useState([]);
+  const [appointmentHistory, setAppointmentHistory] = useState([]);
+
+  const getAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + "/api/doctor/appointmentslist",
+        { headers: { Authorization: `Bearer ${dToken}` } },
+      );
+      if (data.success) {
+        const appointments = data.data;
+        setAppointmentsList(
+          appointments.filter(
+            (appointment) =>
+              appointment.status === "pending" ||
+              appointment.status === "confirmed",
+          ),
+        );
+
+        setAppointmentHistory(
+          appointments.filter(
+            (appointment) =>
+              appointment.status === "completed" ||
+              appointment.status === "cancelled",
+          ),
+        );
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     backendUrl,
     dToken,
     setDToken,
+    appointmentsList,
+    getAppointments,
+    setAppointmentsList,
+    appointmentHistory,
+    setAppointmentHistory,
   };
 
   return (
--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { DoctorContext } from "../../context/DoctorContext";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const DoctorAppointments = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { appointmentsList, dToken, getAppointments, appointmentHistory } =
+    useContext(DoctorContext);
 
-export default DoctorAppointments
+  const [history, setHistory] = useState(false);
+
+  console.log("data:", appointmentsList);
+  console.log("History:", appointmentHistory);
+
+  useEffect(() => {
+    if (dToken) {
+      getAppointments();
+    }
+  }, [dToken]);
+
+  return (
+    appointmentsList && (
+      <div>
+        <div>
+          <div>
+            <h3>{history ? "Appointment History" : "Upcoming Appointments"}</h3>
+          </div>
+          <div>
+            <button onClick={() => setHistory(!history)} type="button">
+              {history ? "Appointments" : "History"}
+            </button>
+          </div>
+        </div>
+        <hr />
+        <div>
+          {history ? (
+            // history
+            <div></div>
+          ) : (
+            // current appointments
+            <div></div>
+          )}
+        </div>
+      </div>
+    )
+  );
+};
+
+export default DoctorAppointments;
